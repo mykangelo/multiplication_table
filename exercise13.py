28dfa766f193c3e32c024e946e83658c56158ed5
mykangelo <mmykangelo@gmail.com>
2024-01-23 21:39:21 +0800

    Answer for exercise number 13

---
+++ exercise13.py
@@ -0,0 +1,12 @@
+ 
+
+
+
+#start a outer  for loop to iterate with a range of 1-10 numbers. 
+for i in range(1, 11):
+    print(" ") # for line seperation
+    for j in range(1, 11): # create a inner loop that iterate also a range of 1-10 numbers
+        print(i * j, end=" ") # print and multiply the inner and outer loop to calculate the multiple of the 2 numbers.
+
+
+        
